8d73f2a08c812b5abf4f1db1d4f17c8b5f39197e
EstebanSalazar-27 <estebanenis27@gmail.com>
2022-07-01 10:33:54 -0300

    cambio

--- src/App.js
+++ src/App.js
@@ -3,44 +3,44 @@ import Navbar from "./components/navbar/navbar";
 import SearchBar from "./components/searchBar/search";
 import Pokedex from "./components/pokedex/pokedex";
 import getPokemons, { getPokemonData } from "./services/PokeApi";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Spinner from "./components/spinner/spinner";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
-  const [loading,setLoading] = useState(true)
-  const [page,setPage] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(0)
   const [total, setTotal] = useState(0)
 
-  const fetchPokemons = async () =>{
-   
-    try{
+  const fetchPokemons = async () => {
+
+    try {
       setLoading(true)
-      const data = await getPokemons(20, 25 * page) 
-      const getPokemonInfo = data.results.map(async (pokemon) =>{
+      const data = await getPokemons(20, 25 * page)
+      const getPokemonInfo = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url)
       })
       const results = await Promise.all(getPokemonInfo)
       setPokemons(results)
       console.log(results)
       setLoading(false)
-      setTotal(Math.ceil(data.count /25))
-    }catch(err){
+      setTotal(Math.ceil(data.count / 25))
+    } catch (err) {
     }
   }
-  
+
 
   useEffect(() => {
     fetchPokemons()
   }, [page]);
-  
- 
+
+
   return (
     <div className="App">
-      <Navbar/>
-      <SearchBar/>
-      
-       <Pokedex page={page} loading={loading} total={total} setPage={setPage} pokemons={pokemons}/>
+      <Navbar />
+      <SearchBar />
+
+      <Pokedex page={page} loading={loading} total={total} setPage={setPage} pokemons={pokemons} />
     </div>
   );
 }
--- src/components/searchBar/search.jsx
+++ src/components/searchBar/search.jsx
@@ -4,27 +4,28 @@ import { searchPokemon } from "../../services/PokeApi";
 
 import "./searchbar.scss"
 
-export default function SearchBar () {
+export default function SearchBar() {
   const [valueSearch, setValueSearch] = useState("")
   const [pokemon, setPokemon] = useState("")
 
-  const  valueToState = (evt)  => {
+  const valueToState = (evt) => {
     setValueSearch(evt.target.value)
   }
 
-  const buscarPokemon = async (e) =>{
+  const buscarPokemon = async (e) => {
     const data = await searchPokemon(valueSearch)
     setPokemon(data)
+   
   }
-  
-  return(
+  console.log(pokemon)
+  return (
     <div className="cont-searchbar">
-      
-      <input type="text" className="searchbar" placeholder="Search Pokemon" onChange={valueToState}/>    
-     
+
+      <input type="text" className="searchbar" placeholder="Search Pokemon" onChange={valueToState} />
+
       <button className="searchbar__btn" onClick={() => buscarPokemon()}>Search</button>
-      
+
     </div>
-    
+
   )
 }
